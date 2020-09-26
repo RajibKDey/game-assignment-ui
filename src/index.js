7d0488728ab3f2977373e58be7b499bac6cd53d0
RajibKDey <RajibKDey@users.noreply.github.com>
2020-09-26 15:00:56 +0530

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "@material-ui/styles";
+import Themes from "./themes";
+
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./services/reducers";
+import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootSaga from "./services/sagas";
+// import Axios from "axios";
+// import { unauthError } from "./serices/login/action";
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+sagaMiddleware.run(rootSaga);
+
+// Axios.interceptors.response.use(
+//   (response) => {
+//     if (false) {
+//       store.dispatch(unauthError());
+//     }
+//     return response;
+//   },
+//   (err) => {
+//     if (false) {
+//       store.dispatch(unauthError());
+//     }
+//     return Promise.reject(err);
+//   }
+// );
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={Themes}>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/services/sagas/index.js
@@ -0,0 +1,7 @@
+import { all } from "redux-saga/effects";
+// import { watchLogin } from '../login/saga';
+// import { watchLogout } from '../login/saga';
+
+export default function* rootSaga() {
+  yield all([]);
+}
